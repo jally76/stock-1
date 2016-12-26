3c845423225e4401b2f926e27d7b1aa754fbc4d2
Артём <artem.v.rusanov@gmail.com>
2016-12-26 20:55:46 +0300

    web service, host, console client

---
+++ Stock.Client.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stock.Client.Console.StockServiceReference;
+
+namespace Stock.Client.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1: Create an instance of the WCF proxy.
+            StockServiceClient client = new StockServiceClient();
+
+            // Step 2: Call the service operations.
+            // Call the Add service operation.
+            var value1 = 100;
+            var result = client.Get(value1);
+            System.Console.WriteLine(result);
+
+            System.Console.ReadLine();
+
+            // Call the Subtract service operation.
+            value1 = 145;
+            result = client.GetAll(value1);
+            System.Console.WriteLine(result);
+
+            System.Console.ReadLine();
+
+            //Step 3: Closing the client gracefully closes the connection and cleans up resources.
+            client.Close();
+
+            System.Console.ReadLine();
+
+        }
+    }
+}
---
+++ Stock.Wcf/StockService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Stock.Wcf
+{
+    public class StockService : IStockService
+    {
+        public string GetAll(int value)
+        {
+            Console.WriteLine(value);
+            return string.Format("You entered: {0}", value);
+        }
+
+        public string Get(int value)
+        {
+            Console.WriteLine(value);
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
